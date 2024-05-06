f54beda9632eeb041eaabc93e9e79f8c22ae2d7b
YoC00lig <kulig.j@vp.pl>
2024-05-06 23:03:53 +0200

    ci-cd

--- src/main/kotlin/inc/evil/Application.kt
+++ src/main/kotlin/inc/evil/Application.kt
@@ -11,12 +11,12 @@ fun main(args: Array<String>) {
 
 // przykład tylko, ale działający u mnie ;>
 fun Application.module() {
-    Database.connect(
-            "jdbc:postgresql://localhost:5432/d17_map",
-            driver = "org.postgresql.Driver",
-            user = "d17_map",
-            password = System.getenv("POSTGRES_PASSWORD")
-        )
+//    Database.connect(
+//            "jdbc:postgresql://localhost:5432/d17_map",
+//            driver = "org.postgresql.Driver",
+//            user = "d17_map",
+//            password = System.getenv("POSTGRES_PASSWORD")
+//        )
 
 
 //    configureSerialization()
