16c0e923cec2228328e05aa445c3e70938231a0f
Adam Mężydło <adamkmezydlo@gmail.com>
2024-10-07 22:25:36 +0200

    Create new Spring Application

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,79 +1,51 @@
-@file:Suppress("PropertyName")
-
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-
-val postgresql_driver_version: String by project
-val exposed_version: String by project
-val h2_version : String by project
-
 plugins {
-    kotlin("jvm") version "1.9.23"
-    id("io.ktor.plugin") version "2.3.10"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.23"
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.3.4"
+    id("io.spring.dependency-management") version "1.1.6"
+    kotlin("plugin.jpa") version "1.9.25"
 }
 
 group = "inc.evil"
-version = "0.0.1"
+version = "0.0.1-SNAPSHOT"
 
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
 
-application {
-    mainClass.set("io.ktor.server.netty.EngineMain")
-
-    val isDevelopment: Boolean = project.ext.has("development")
-    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 repositories {
     mavenCentral()
 }
 
-
 dependencies {
-    // Core Ktor
-    implementation("io.ktor:ktor-server-core-jvm")
-    implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("io.ktor:ktor-server-config-yaml:2.3.10")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("io.ktor:ktor-client-core:$ktor_version")
-    implementation("io.ktor:ktor-client-cio:$ktor_version")
-
-    // Dependency injection
-    implementation("io.insert-koin:koin-ktor:3.5.6")
-    implementation("io.insert-koin:koin-core:3.5.6")
-
-    // Serialization
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm")
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.0")
-
-    // Postgres
-    implementation("org.postgresql:postgresql:$postgresql_driver_version")
-
-    // Exposed
-    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-java-time:$exposed_version")
-
-    // Swagger UI
-    implementation("io.ktor:ktor-server-swagger:$ktor_version")
-
-    // CORS
-    implementation("io.ktor:ktor-server-cors:$ktor_version")
-
-    // Tests
-    testImplementation("io.ktor:ktor-server-tests-jvm")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
-
-
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("org.postgresql:postgresql")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-sourceSets {
-    main {
-        kotlin {
-            exclude("/home/amezydlo/AGH/sem6/PP/D17Map/D17Map-Backend/src/main/kotlin/inc/evil/CelePogladowe.kt")
-        }
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
     }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
--- src/main/kotlin/inc/evil/persistance/repositories/ClassroomRepository.kt
+++
@@ -1,8 +0,0 @@
-package inc.evil.persistance.repositories
-
-import inc.evil.persistance.entities.Classroom
-import java.util.UUID
-
-interface ClassroomRepository : Repository<UUID, Classroom> {
-    fun findByName(name: String): List<Classroom>
-}
