469a3d0a4f4befdac020f6d91a2349f0996dda74
Adam Mężydło <adamkmezydlo@gmail.com>
2024-10-14 21:33:33 +0200

    EVIL-7 Implement JWT logic

---
+++ src/main/kotlin/inc/evil/d17map/security/jwt/JWTFilter.kt
@@ -0,0 +1,53 @@
+package inc.evil.d17map.security.jwt
+
+
+import inc.evil.d17map.security.user.BasicUserDetailsService
+import jakarta.servlet.FilterChain
+import jakarta.servlet.ServletException
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.context.ApplicationContext
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import java.io.IOException
+
+@Component
+class JWTFilter(
+    private val jwtService: JWTService,
+    private val applicationContext: ApplicationContext
+) : OncePerRequestFilter() {
+
+    @Throws(ServletException::class, IOException::class)
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val authHeader = request.getHeader("Authorization")
+        var token: String? = null
+        var username: String? = null
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            token = authHeader.substring(7)
+            username = jwtService.extractUsername(token)
+        }
+
+        if (username != null && SecurityContextHolder.getContext().authentication == null) {
+            val userDetails: UserDetails =
+                applicationContext.getBean(BasicUserDetailsService::class.java).loadUserByUsername(username)
+
+            if (token != null && jwtService.validateToken(token, userDetails)) {
+                val usernamePasswordAuthenticationToken = UsernamePasswordAuthenticationToken(
+                    userDetails, null, userDetails.authorities
+                )
+                usernamePasswordAuthenticationToken.details = WebAuthenticationDetailsSource().buildDetails(request)
+                SecurityContextHolder.getContext().authentication = usernamePasswordAuthenticationToken
+            }
+        }
+        filterChain.doFilter(request, response)
+    }
+}
