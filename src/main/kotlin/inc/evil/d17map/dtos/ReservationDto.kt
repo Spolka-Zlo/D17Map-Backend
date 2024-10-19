60beb282bbeeefd71c4d802b66bebd108daa429c
Adam Mężydło <adamkmezydlo@gmail.com>
2024-10-19 21:52:22 +0200

    EVIL-11 Add JsonFormat to date-time related fields in DTOs

--- src/main/kotlin/inc/evil/d17map/dtos/ReservationDto.kt
+++ src/main/kotlin/inc/evil/d17map/dtos/ReservationDto.kt
@@ -1,6 +1,8 @@
 package inc.evil.d17map.dtos
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import inc.evil.d17map.enums.ReservationType
+import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDate
 import java.time.LocalTime
 import java.util.*
@@ -8,8 +10,17 @@ import java.util.*
 data class ReservationDto(
     val id: UUID? = null,
     val title: String,
+
+    @Schema(type = "string",  example = "24-12-2024")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     val date: LocalDate,
+
+    @Schema(type = "string", format="time", example = "11:30")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
     val startTime: LocalTime,
+
+    @Schema(type = "string", format="time", example = "13:00")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
     val endTime: LocalTime,
     val classroomId: UUID? = null,
     val classroom: ClassroomDto? = null,
