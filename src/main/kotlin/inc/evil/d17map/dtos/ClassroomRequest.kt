1006abf8446f3647367c72a4a35415a87a19b011
YoC00lig <kulig.j@vp.pl>
2024-11-09 16:00:37 +0100

    conflicts resolved

--- src/main/kotlin/inc/evil/d17map/dtos/ClassroomRequest.kt
+++ src/main/kotlin/inc/evil/d17map/dtos/ClassroomRequest.kt
@@ -1,6 +1,5 @@
 package inc.evil.d17map.dtos
 
-import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Positive
 import jakarta.validation.constraints.Size
 import java.util.*
