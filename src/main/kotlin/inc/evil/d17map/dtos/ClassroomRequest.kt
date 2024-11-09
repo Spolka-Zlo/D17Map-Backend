aab7ee126a7a6263b81f94ae36c82a778e109d5d
YoC00lig <kulig.j@vp.pl>
2024-11-09 14:58:05 +0100

    removed unnecessary annotations

--- src/main/kotlin/inc/evil/d17map/dtos/ClassroomRequest.kt
+++ src/main/kotlin/inc/evil/d17map/dtos/ClassroomRequest.kt
@@ -9,9 +9,7 @@ data class ClassroomRequest(
     @field:Size(min = 1, max = 100, message = "Classroom name must be between 1 and 100 characters long.")
     val name: String,
 
-    @field:NotBlank(message = "Description must not be blank.")
     @field:Size(min = 1, max = 255, message = "Description must be between 1 and 255 characters long.")
-    @field:Size(max = 255, message = "Description must not exceed 255 characters.")
     val description: String,
 
     @field:Positive(message = "Capacity must be a positive number.")
