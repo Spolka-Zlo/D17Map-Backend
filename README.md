09bf5dc7b4578be115cc7015050bab7952934707
Adam Mężydło <adamkmezydlo@gmail.com>
2024-11-30 00:49:33 +0100

    EVIL-19 Prepare application for release & deployment

--- README.md
+++ README.md
@@ -52,3 +52,20 @@ Other useful commands may be found [here](https://contabo.com/blog/how-to-remove
 
 ![Object Model](docs/object_model.png)
 
+
+
+
+# IMPORTANT FOR BACKEND DEVS
+
+## Running locally with H2 DEV database
+
+1. `./gradlew clean build`
+2. Click on the green arrow in Intellij to start :)
+
+> NOTE: Remember to leave 'spring.profiles.active=dev' in application.properties file
+
+## Running locally with Postgresql PROD database located in Azure
+
+1. `./gradlew clean bootJar`
+2. `docker build -t <your_container_name> .`
+3. 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
-    kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "3.3.4"
+    kotlin("jvm") version "2.1.0"
+    kotlin("plugin.spring") version "2.1.0"
+    id("org.springframework.boot") version "3.4.0"
     id("io.spring.dependency-management") version "1.1.6"
-    kotlin("plugin.jpa") version "1.9.25"
+    kotlin("plugin.jpa") version "2.1.0"
+    id("net.researchgate.release") version "3.0.2"
 }
 
 group = "inc.evil"
@@ -66,4 +67,13 @@ kotlin {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+release {
+    failOnCommitNeeded = true
+    failOnPublishNeeded = true
+    failOnSnapshotDependencies = true
+    preCommitText = "[RELEASE]"
+    tagTemplate = "v${version}"
+    versionPropertyFile = "build.gradle.kts"
 }
