b2fc516a419971e204c1cb8b3b461a29d00d686f
Adam Mężydło <adamkmezydlo@gmail.com>
2024-11-30 02:41:34 +0100

    EVIL-19 revert springboot  dependency update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    kotlin("jvm") version "2.1.0"
-    kotlin("plugin.spring") version "2.1.0"
-    id("org.springframework.boot") version "3.4.0"
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.3.4"
     id("io.spring.dependency-management") version "1.1.6"
-    kotlin("plugin.jpa") version "2.1.0"
+    kotlin("plugin.jpa") version "1.9.25"
     id("net.researchgate.release") version "3.0.2"
 }
 
