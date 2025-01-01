b4236c852bb4d24a9a5fc0db4d7c043d1f2dac98
Adam Mężydło <adamkmezydlo@gmail.com>
2025-01-01 15:43:42 +0100

    EVIL-93 Remove useless classes

--- src/main/kotlin/inc/evil/d17map/security/authorization/PermissionRepository.kt
+++
@@ -1,7 +0,0 @@
-package inc.evil.d17map.security.authorization
-
-import org.springframework.data.jpa.repository.JpaRepository
-import java.util.*
-
-interface PermissionRepository : JpaRepository<Permission, UUID> {
-}
--- src/main/kotlin/inc/evil/d17map/security/authorization/test/TestBody.kt
+++
@@ -1,3 +0,0 @@
-package inc.evil.d17map.security.authorization.test
-
-data class TestBody(val one: String, val two: String, val three: String, val four: String, val five: String)
